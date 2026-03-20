8172c07a4ccf335f26f46769f20b383dbdda0ef8
Gabriel Henrique da Costa Tavares <henriquepokacha@gmail.com>
2026-03-20 12:55:39 -0300

    reset

--- frontend/src/components/GradientDivider.jsx
+++
@@ -1,6 +0,0 @@
-// src/components/GradientDivider.jsx
-import './GradientDivider.css'
-
-export default function GradientDivider() {
-  return <div className="gradient-divider"></div>
-}
--- frontend/src/utils/animations.js
+++
@@ -1,9 +0,0 @@
-export function animateOnLoad(selector, className = 'show', delay = 0) {
-  const elements = document.querySelectorAll(selector)
-
-  elements.forEach((el, index) => {
-    setTimeout(() => {
-      el.classList.add(className)
-    }, delay * index)
-  })
-}
