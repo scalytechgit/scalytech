484f08bde63a6de6c71094637076f322182f56e3
Nícolas Francisco Martins Chagas <125323611+Nickfmc07@users.noreply.github.com>
2026-03-23 22:03:00 -0300

    Testando  hash router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // <-- Mudei para HashRouter
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ChatBot from './components/ChatBot'; // <-- adicionado
+import ChatBot from './components/ChatBot';
 
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/scalytech">
+    <HashRouter>
       <Header />
 
       <Routes>
@@ -20,6 +20,6 @@ export default function App() {
       <Footer />
 
       <ChatBot /> {/* <-- chatbot fixo no canto */}
-    </BrowserRouter>
+    </HashRouter>
   );
 }
