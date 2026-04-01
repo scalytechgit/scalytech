5a1e1af98a50d35515420d77f24e25ef3d68b45a
Nícolas Francisco Martins Chagas <125323611+Nickfmc07@users.noreply.github.com>
2026-04-01 13:34:15 -0300

    fix router base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter /* basename={import.meta.env.BASE_URL} */>
       <Header />
 
       <Routes>
